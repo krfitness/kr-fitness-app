a4c6b7e1ca87cded887546d0e00c8852a603273b
Your Name <your@email.com>
2026-07-15 17:13:47 +0530

    Fix updateClient export

--- src/features/clients/services/updateClientService.js
+++ src/features/clients/services/updateClientService.js
@@ -1,8 +1,8 @@
 import { db } from "../../../firebase/firebaseConfig";
 import { doc, updateDoc } from "firebase/firestore";
 
-export async function updateClientStatus(id, status) {
-  await updateDoc(doc(db, "users", id), {
-    status,
-  });
+export async function updateClient(id, data) {
+  const ref = doc(db, "clients", id);
+
+  await updateDoc(ref, data);
 }
